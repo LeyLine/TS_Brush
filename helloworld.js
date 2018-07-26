a373a83059371dd8286c4e47e831f20458b1b160
Adrian Llerena <a.llerena001@gmail.com>
2018-07-26 18:14:10 -0400

    first

---
+++ helloworld.js
@@ -0,0 +1,11 @@
+class Greeter {
+    constructor(message) {
+        this.message = message;
+    }
+    sayHi() {
+        return "<h1>" + this.message + "</h1>";
+    }
+}
+let greeter = new Greeter("Hello, world!");
+document.body.innerHTML = greeter.sayHi();
+//# sourceMappingURL=helloworld.js.map
